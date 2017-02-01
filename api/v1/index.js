9ac76d2bf12c509fb2dffc7538432a128cf126da
X140Yu <zhaoxinyu1994@gmail.com>
2017-02-01 21:11:34 +0800

    Better API routes

--- api/v1/index.js
+++ api/v1/index.js
@@ -1,9 +1,18 @@
-var v1Path = '/api/v1';
+
+// 如果需要加入新的请求路径，只需要在下面加入路径
+// 然后在 `api/v1` 下建立同名文件即可
+let ROUTE_PATHS = [
+  '/signin',
+  '/signup',
+  '/diary'
+];
 
 module.exports = function (app) {
 
-  app.use(v1Path + '/signin', require('./signin'));
-  app.use(v1Path + '/signup', require('./signup'));
-  app.use(v1Path + '/diary', require('./diary'));
+  for (let i in ROUTE_PATHS) {
+    let path = ROUTE_PATHS[i];
+    console.log(path);
+    app.use('/api/v1' + path, require('.' + path));
+  }
 
 };
