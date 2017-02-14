e9d233b9e270cbb7c2391c211bac8ad5633ecb19
X140Yu <zhaoxinyu1994@gmail.com>
2017-02-14 22:49:46 +0800

    Remove express-formidable use body-parser instead
    express-formidable causes the `Can’t set headers after they are sent.`
    error

--- index.js
+++ index.js
@@ -15,21 +15,24 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 // 设置模板引擎为 ejs
 app.set('view engine', 'ejs');
+// disable 304
+// app.disable('etag');
 
 // 设置静态文件目录
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 /*
-  // API
+// API
   如果需要新增加一个 API 的版本
   1. 把 ./api/v1 目录复制一份，改名为 v2
   2. 在下面的 API_VERSIONS 字典中加入一对 kv，如 `'Version 2': '/v2'`
   其它的事情就不需要担心啦
- */
+*/
 var API_VERSIONS = {
   'Version 1': '/v1'
-}
+};
 for (var v in API_VERSIONS) {
   var api = require('./api' + API_VERSIONS[v]);
   api(app);
@@ -50,20 +53,15 @@ app.use(session({
 // flash 中间价，用来显示通知
 app.use(flash());
 
-// 处理表单及文件上传的中间件
-app.use(require('express-formidable')({
-  uploadDir: path.join(__dirname, 'public/images'),// 上传文件目录
-  keepExtensions: true// 保留后缀
-}));
-
 // 设置模板全局常量
 app.locals.website = {
   title: "DayCache",
-  description: "A missing diary taking website." 
+  description: "A missing diary taking website."
 };
 
 // 添加模板必需的三个变量
 app.use(function (req, res, next) {
+  console.log("模版 常用");
   res.locals.user = req.session.user;
   res.locals.success = req.flash('success').toString();
   res.locals.error = req.flash('error').toString();
@@ -73,24 +71,43 @@ app.use(function (req, res, next) {
 // 路由
 routes(app);
 
-// 监听端口，启动程序
-app.listen(config.port, function () {
-  console.log(`${pkg.name} listening on port ${config.port}`);
-});
-
 // 捕获 404 并定向到错误处理
 app.use(function(req, res, next) {
+  return res.send('404');
+  console.log("error!!!!! 4004");
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// 开发环境下的错误处理
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
-
     res.status(err.status || 500);
     // 如果 path 包含 'api' 会返回 json
+    if (req.path.includes('api')) {
+      console.log('api-wrong');
+      return res.json({
+        status: {
+          code: err.status,
+          message: err.message
+        },
+        data: { }
+      });
+    }
+
+    return res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// 生产环境下的错误处理
+if (app.get('env') !== 'development') {
+  app.use(function(err, req, res, next) {
+    // 设置响应状态
+    res.status(err.status || 500);
+
     if (req.path.includes('api')) {
       return res.json( {
         status: {
@@ -101,31 +118,16 @@ if (app.get('env') === 'development') {
       });
     }
 
-    res.render('error', {
+    // 渲染错误处理页
+    return res.render('error', {
       message: err.message,
-      error: err,
+      error: {}
     });
   });
 }
 
-// 生产环境下的错误处理
-app.use(function(err, req, res, next) {
-  // 设置响应状态
-  res.status(err.status || 500);
-
-  if (req.path.includes('api')) {
-    return res.json( {
-      status: {
-        code: err.status,
-        message: err.message
-      },
-      data: { }
-    });
-  }
-
-  // 渲染错误处理页
-  res.render('error', {
-    message: err.message,
-    error: {},
-  });
+// 监听端口，启动程序
+app.listen(config.port, function () {
+  console.log(`${pkg.name} listening on port ${config.port}`);
 });
+
